18c6004a4571905f01988b4e34f89e2694c288f2
Muhammed Shittu <coolzyte@gmail.com>
2021-10-07 18:45:07 +0100

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import "./App.css";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hellooo</header>
+      <header className="App-header">
+        <Home />
+      </header>
     </div>
   );
 }
